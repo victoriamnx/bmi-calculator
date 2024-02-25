7535f7daba938ebbb924d1fb7ad9f3234c609640
Victoria Nogueira Marques <victoriamarques0604@gmail.com>
2024-02-25 14:59:22 -0300

    add: infos

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
+import Logo from "./components/images/logo.svg";
+
 function App() {
   return (
-    <div>
-      <img src="" alt="" />
-      <h1>Body Mass Index Calculator</h1>
-      <p>
+    <div className="flex flex-col px-6 items-center">
+      <img src={Logo} alt="Logo" className="w-10 h-10 mt-8"/>
+      <h1 className="font-inter font-semibold text-[#253347] text-5xl	text-center m-6">Body Mass Index Calculator</h1>
+      <p className="font-inter font-normal text-[#5E6E85] text-base	 text-center">
         Better understand your weight in relation to your height using our body
         mass index (BM) calculator. While BMI is not the sole determinant of a
         healthy weight, it offers a valuable starting point to evaluate your
