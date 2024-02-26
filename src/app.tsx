e0978de5e3d6e84b80b4f556c504e78786312086
Victoria Nogueira Marques <victoriamarques0604@gmail.com>
2024-02-26 20:44:04 -0300

    add: new parts

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,16 @@
 import Logo from "./components/images/logo.svg";
+import Photo from "./components/images/image-man-eating.webp";
+import HealthyEat from "./components/images/icon-eating.svg";
+import Exercise from "./components/images/icon-exercise.svg";
+import Sleep from "./components/images/icon-sleep.svg";
 
 function App() {
   return (
-    <div className="w-screen h-screen flex flex-col bg-[#D8E2E7] ">
-      <div className="flex flex-col px-6 items-center ">
+    <div className="w-screen h-screen flex flex-col bg-[#ffff] ">
+      <div
+        id="init"
+        className="flex flex-col px-6 items-center rounded-b-[35px] overflow-visible mb-[450px]"
+      >
         <img src={Logo} alt="Logo" className="w-10 h-10 mt-8" />
         <h1 className="font-inter font-semibold text-[#253347] text-5xl	text-center m-6 antialiased">
           Body Mass Index Calculator
@@ -15,7 +22,10 @@ function App() {
           your overall health and well-being.
         </p>
 
-        <div className="bg-white p-6 rounded-2xl mt-12 mb-12 flex flex-col gap-6">
+        <div
+          id="result"
+          className="bg-white p-6 rounded-2xl mt-12 mb-12 flex flex-col gap-6 relative"
+        >
           <div className="flex flex-col gap-6">
             <h1 className="font-inter font-semibold text-2xl text-[#253347] antialiased">
               Enter your details below
@@ -31,29 +41,95 @@ function App() {
                 Imperial
               </label>
             </div>
-          </div>    
+          </div>
           <div className="flex flex-col ">
             <form action="" className="flex flex-col">
               <label className="font-inter font-normal text-sm	text-[#5E6E85] mb-2">
                 Height
               </label>
-              <input type="number" id="" name="" value="" className="border p-5 rounded-xl"/>
+              <input
+                type="number"
+                id=""
+                name=""
+                value=""
+                className="border p-5 rounded-xl"
+              />
               <label className="font-inter font-normal text-sm	text-[#5E6E85] mt-4 mb-2">
                 Weight
               </label>
-              <input type="number" id="" name="" value="" className="border p-5 rounded-xl"/>
+              <input
+                type="number"
+                id=""
+                name=""
+                value=""
+                className="border p-5 rounded-xl"
+              />
             </form>
           </div>
 
-          <div id="result" className="">
-            <h1> Your BMI is...</h1>
-            <span>result </span>
-            <p>
+          <div className="flex flex-col p-8 bg-[#345FF6] rounded-2xl gap-6">
+            <div>
               {" "}
+              <h1 className="font-inter font-semibold text-base text-white">
+                {" "}
+                Your BMI is...
+              </h1>
+              <span className="font-inter font-semibold text-5xl text-white">
+                result{" "}
+              </span>
+            </div>
+            <p className="font-inter font-normal text-sm text-white">
               Your BMI suggests you're add classification Your ideal weight is
               between add range
             </p>
           </div>
+        </div>
+      </div>
+
+      <div className="w-screen h-auto flex flex-col">
+        <img src={Photo} alt="" />
+        <div className="flex flex-col px-6 gap-8 mb-16">
+          <h1 className="font-inter font-semibold text-[#253347] text-3xl antialiased mt-12">
+            What your BMI result means
+          </h1>
+          <p className="font-inter font-normal text-[#5E6E85] text-base antialiased">
+            A BMI range of 18.5 to 24.9 is considered a 'healthy weight.'
+            Maintaining a healthy weight may lower your chances of experiencing
+            health issues later on, such as obesity and type 2 diabetes. Aim for
+            a nutritious diet with reduced fat and sugar content, incorporating
+            ample fruits and vegetables. Additionally, strive for regular
+            physical activity, ideally about 30 minutes daily for five days a
+            week.
+          </p>
+        </div>
+      </div>
+
+      <div id="benefits" className="w-screen flex flex-col gap-10 px-6 py-14">
+        <div className="flex flex-col gap-6">
+          <img src={HealthyEat} alt="" className="w-16 h-16 " />
+          <h1> Healthy eating</h1>
+          <p>
+            Healthy eating promotes weight control, disease prevention, better
+            digestion, immunity, mental clarity, and mood.
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-6">
+          <img src={Exercise} alt="" className="w-16 h-16 " />
+          <h1> Regular exercise</h1>
+          <p>
+            Exercise improves fitness, aids weight control, elevates mood, and
+            reduces disease risk, fostering wellness and longevity.
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-6">
+          <img src={Sleep} alt="" className="w-16 h-16 " />
+          <h1> Adequate sleep</h1>
+          <p>
+            Sleep enhances mental clarity, emotional stability, and physical
+            wellness, promoting overall restoration and rejuvenation.
+          </p>
         </div>
       </div>
     </div>
